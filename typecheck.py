0cddd897d6b9728bfdaa4c1d341709f2614fa3fb
vinodh1988 <k.c.vinodh@hotmail.com>
2022-09-19 10:54:11 +0530

    type-commit

---
+++ typecheck.py
@@ -0,0 +1,12 @@
+def takeparamonlystring(a):
+    if(isinstance(a, int)):
+        print('int is ',a)
+    else:
+        print('only int accepted')
+
+
+takeparamonlystring('Rahul')
+takeparamonlystring(13)
+takeparamonlystring(53.3)
+
+    
