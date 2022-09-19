20673ba281208da014f2c5fcda935050574bd5db
vinodh1988 <k.c.vinodh@hotmail.com>
2022-09-19 11:23:45 +0530

    set

--- collections.py
+++ collections.py
@@ -14,3 +14,15 @@ print(a[1:6:2]) # in step of 2
 print(a[::-1])
 print(a[-1:-5:-1])
 
+b=(1,2,536,34) #Immutable - tuple of
+               # same as list but cannot be changed[immutable]
+
+print(b)
+print(b[2])
+
+c= {1,35,56,35,647,23,266,23} # set wont allow duplicates
+                              # set is not ordered and not indexed
+
+c.add(50)
+
+print(c)
