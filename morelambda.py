bfa0050b40a2a996f55d63a3d22c66fe4bd7f07a
vinodh1988 <k.c.vinodh@hotmail.com>
2022-09-20 10:34:10 +0530

    lambda

---
+++ morelambda.py
@@ -0,0 +1,14 @@
+def sum(a,b):
+    return a+b
+
+add=sum
+
+print(add(535,34))
+
+addition=lambda a,b:a+b
+
+print(addition(535,34))
+
+whichisgreater=lambda a,b: a if a>b else b
+
+print(whichisgreater(35,646))
